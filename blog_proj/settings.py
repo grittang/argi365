56ba6eb3fa8dbedf5976b074368c0193ac178c71
grittang <grittang@gmail.com>
2021-05-01 14:49:02 +0800

    add parameters to pipfile

--- blog_proj/settings.py
+++ blog_proj/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'f
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, 'static')
