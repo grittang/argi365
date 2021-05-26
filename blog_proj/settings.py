ef019fc47e52f3d3046f64c868b3f9bde4ac5251
grittang <grittang@gmail.com>
2021-05-26 22:54:50 +0800

    debug

--- blog_proj/settings.py
+++ blog_proj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-)5xeircrhxk*(!ufwdq9=9bisby17*!b3lqka*ppm2gxnfa4!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.argi365.com']
 
